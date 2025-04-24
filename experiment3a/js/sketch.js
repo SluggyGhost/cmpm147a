2652e56f2b33d6db09e677f584b9fc2f1599007b
Josh Acosta <jodacost@ucsc.edu>
2025-04-23 23:59:53 -0700

    Trying to get the canvas to appear.

--- experiment3a/js/sketch.js
+++ experiment3a/js/sketch.js
@@ -1,79 +1,223 @@
-// sketch.js - purpose and description here
-// Author: Your Name
-// Date:
+// Author: Joshua Acosta
+// Date: 4/23/25
 
-// Here is how you might set up an OOP p5.js project
-// Note that p5.js looks for a file called sketch.js
+/* exported preload, setup, draw, placeTile */
 
-// Constants - User-servicable parts
-// In a longer project I like to put these in a separate file
-const VALUE1 = 1;
-const VALUE2 = 2;
+/* global generateGrid drawGrid */
 
-// Globals
-let myInstance;
-let canvasContainer;
-var centerHorz, centerVert;
+let seed = 0;
+let tilesetImage;
+let currentGrid = [];
+let numRows, numCols;
 
-class MyClass {
-    constructor(param1, param2) {
-        this.property1 = param1;
-        this.property2 = param2;
-    }
-
-    myMethod() {
-        // code to run when method is called
-    }
+function preload() {
+  tilesetImage = loadImage(
+    "https://cdn.glitch.com/25101045-29e2-407a-894c-e0243cd8c7c6%2FtilesetP8.png?v=1611654020438"
+  );
 }
 
-function resizeScreen() {
-  centerHorz = canvasContainer.width() / 2; // Adjusted for drawing logic
-  centerVert = canvasContainer.height() / 2; // Adjusted for drawing logic
-  console.log("Resizing...");
-  resizeCanvas(canvasContainer.width(), canvasContainer.height());
-  // redrawCanvas(); // Redraw everything based on new size
+function reseed() {
+  seed = (seed | 0) + 1109;
+  randomSeed(seed);
+  noiseSeed(seed);
+  select("#seedReport").html("seed " + seed);
+  regenerateGrid();
 }
 
-// setup() function is called once when the program starts
+function regenerateGrid() {
+  select("#asciiBox").value(gridToString(generateGrid(numCols, numRows)));
+  reparseGrid();
+}
+
+function reparseGrid() {
+  currentGrid = stringToGrid(select("#asciiBox").value());
+}
+
+function gridToString(grid) {
+  let rows = [];
+  for (let i = 0; i < grid.length; i++) {
+    rows.push(grid[i].join(""));
+  }
+  return rows.join("\n");
+}
+
+function stringToGrid(str) {
+  let grid = [];
+  let lines = str.split("\n");
+  for (let i = 0; i < lines.length; i++) {
+    let row = [];
+    let chars = lines[i].split("");
+    for (let j = 0; j < chars.length; j++) {
+      row.push(chars[j]);
+    }
+    grid.push(row);
+  }
+  return grid;
+}
+
 function setup() {
-  // place our canvas, making it fit our container
   canvasContainer = $("#canvas-container");
   let canvas = createCanvas(canvasContainer.width(), canvasContainer.height());
   canvas.parent("canvas-container");
-  // resize canvas is the page is resized
-
-  // create an instance of the class
-  myInstance = new MyClass("VALUE1", "VALUE2");
-
   $(window).resize(function() {
     resizeScreen();
   });
   resizeScreen();
+   $("#reimagine").click(() => seed++);
+
+  reseed();
 }
 
-// draw() function is called repeatedly, it's the main animation loop
 function draw() {
-  background(220);    
-  // call a method on the instance
-  myInstance.myMethod();
-
-  // Set up rotation for the rectangle
-  push(); // Save the current drawing context
-  translate(centerHorz, centerVert); // Move the origin to the rectangle's center
-  rotate(frameCount / 100.0); // Rotate by frameCount to animate the rotation
-  fill(234, 31, 81);
-  noStroke();
-  rect(-125, -125, 250, 250); // Draw the rectangle centered on the new origin
-  pop(); // Restore the original drawing context
-
-  // The text is not affected by the translate and rotate
-  fill(255);
-  textStyle(BOLD);
-  textSize(140);
-  text("p5*", centerHorz - 105, centerVert + 40);
+  randomSeed(seed);
+  drawGrid(currentGrid);
 }
 
-// mousePressed() function is called once after every time a mouse button is pressed
-function mousePressed() {
-    // code to run when mouse is pressed
+function placeTile(i, j, ti, tj) {
+  image(tilesetImage, 16 * j, 16 * i, 16, 16, 8 * ti, 8 * tj, 8, 8);
 }
+
+function resizeScreen() {
+  let w = canvasContainer.width();
+  let h = canvasContainer.height();
+  if (h === 0) h = 400;
+  resizeCanvas(w, h);
+}
+
+function generateGrid(numCols, numRows) {
+  // Generate the grid
+  let grid = [];
+  for (let i = 0; i < numRows; i++) {
+    let row = [];
+    for (let j = 0; j < numCols; j++) {
+      row.push("_");
+    }
+    grid.push(row);
+  }
+  
+  let maxRooms = 5;
+  generateRooms(grid, maxRooms);
+  
+  return grid;
+}
+
+function drawGrid(grid) {
+  background(128);
+
+  // Ground
+  for(let i = 0; i < grid.length; i++) {
+    for(let j = 0; j < grid[i].length; j++) {
+      if(gridCheck(grid, i, j, '_')){
+        placeTile(i, j, random(4) | 0, 0);
+        // drawContext(grid, i, j, '.', 5, 4);
+      } else {
+        placeTile(i, j, random(4) | 0, 3);
+        drawContext(grid, i, j, '_', 5, 1);
+      }
+    }
+  }
+  
+  // Objects
+  let maxObjects = 30;
+  for(let i = 0; i < maxObjects; i++){
+    let row = floor(random(grid.length));
+    let col = floor(random(grid[0].length));
+    if(gridCheck(grid, row, col, '_')){
+      placeTile(row,col,14,0);  // Tree (on the grass)
+    } else if (gridCheck(grid, row, col, '.')){
+      placeTile(row,col,26,floor(random(4)));  // House
+    }
+  }
+  
+  // Tower (2 tile object)
+  let towerType = 28 + floor(random(3));
+  let row = floor(random(grid.length));
+  let col = floor(random(grid[0].length));
+  placeTile(row+1,col,towerType,1);// Tower base
+  placeTile(row,col,towerType,0);  // Tower top
+  
+  // Clouds
+  let noiseLevel = 255;
+  let noiseScale = 0.009;
+  for (let y = 0; y < height; y += 1) {
+    for (let x = 0; x < width; x += 1) {
+      let nx = noiseScale * x;
+      let ny = noiseScale * y;
+      let nt = noiseScale * frameCount;
+
+      let c = noiseLevel * noise(nx, ny, nt);
+
+      stroke(c,c,c,180);
+      point(x, y);
+    }
+  }
+}
+
+function generateRooms(grid, maxRooms){
+  for(let i = 0; i < maxRooms; i++){
+    // Get the corners of the room
+    // (Also ensures that the room is 2x2 at minimum.)
+    let row1 = floor(random(grid.length-1));
+    let row2 = floor(random(grid.length));
+    if(row1 > row2){[row1, row2] = [row2, row1];}
+    if(row1 == row2){row2++;}
+    let column1 = floor(random(grid[0].length-1));
+    let column2 = floor(random(grid[0].length));
+    if(column1 > column2){[column1, column2] = [column2, column1];}
+    if(column1 == column2){column2++;}
+    
+    // Generate the room
+    for(let i = row1; i <= row2; i++){
+      for(let j = column1; j <= column2; j++){
+        grid[i][j] = '.';
+      }
+    }
+  }
+}
+
+function gridCheck(grid, i, j, target) {
+  // Check if target is at grid[i][j]
+  return (
+    i >= 0 && i < grid.length &&
+    j >= 0 && j < grid[i].length &&
+    grid[i][j] === target
+  );
+}
+
+function gridCode(grid, i, j, target) {
+  // Form a 4-digit code from neighbors of grid[i][j]
+  let northBit, southBit, eastBit, westBit;
+  northBit = southBit = eastBit = westBit = false;
+  if(gridCheck(grid, i-1, j, target)){northBit = true;}
+  if(gridCheck(grid, i, j+1, target)){eastBit = true;}
+  if(gridCheck(grid, i+1, j, target)){southBit = true;}
+  if(gridCheck(grid, i, j-1, target)){westBit = true;}
+  
+  let code = (westBit << 0) + (southBit << 1) + (eastBit << 2) + (northBit << 3);
+  return code;
+}
+
+function drawContext(grid, i, j, target, ti, tj) {
+  let code = gridCode(grid, i, j, target);
+  const [tiOffset, tjOffset] = lookup[code];
+  placeTile(i, j, ti + tiOffset, tj + tjOffset);
+}
+
+const lookup = [  // [right/left,down/up]
+  [0,0],   //  0, code:0000
+  [-1,0],  //  1, code:0001
+  [0,1],   //  2, code:0010
+  [-1,1],  //  3, code:0011
+  [1,0],   //  4, code:0100
+  [0,0],   //  5, code:0101
+  [1,1],   //  6, code:0110
+  [0,0],    //  7, code:0111, NO TILE
+  [0,-1],  //  8, code:1000
+  [-1,-1], //  9, code:1001
+  [0,0],    // 10, code:1010
+  [0,0],    // 11, code:1011, NO TILE
+  [1,-1],  // 12, code:1100
+  [0,0],    // 13, code:1101, NO TILE
+  [0,0],    // 14, code:1110, NO TILE
+  [0,0]    // 15, code:1111
+];
